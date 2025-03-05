4fe0e58a9be18755bd33a3da1dea718814045b9e
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-03-05 11:52:18 +0530

    new build changes

--- src/MainComp/Router/index.jsx
+++ src/MainComp/Router/index.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePageMainComponent from "../HomePageMain/HomePageMain";
 import AboutUsMain from "../AboutUsMain/AboutUsMain";
 import HeaderComponent from "../../Components/HeaderComp/header";
-import ContactUsMain from "../../MainComp/contactUsMain/contactUsMain"
+import ContactUsMain from "../../MainComp/contactUsMain/contactUsMain";
 import Services from "../../Components/Services/services";
 
 const AllRoute = () => {
   return (
     <Router>
       {/* <HeaderComponent/> */}
-        <Routes>
-            <Route path="/" element={<HomePageMainComponent/>}/>
-            {/* <Route path="/aboutUs" element={<AboutUsMain/>}/>
-            <Route path="/contactus" element={<ContactUsMain/>}/>
-            <Route path="/services" element={<Services/>}/> */}
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePageMainComponent />} />
+        {/* <Route path="/aboutUs" element={<AboutUsMain />} />
+        <Route path="/contactus" element={<ContactUsMain />} />
+        <Route path="/services" element={<Services />} /> */}
+      </Routes>
     </Router>
-  )
+  );
 };
 
 export default AllRoute;
